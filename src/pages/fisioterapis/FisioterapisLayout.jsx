55c00d6ef9fcb47cbaf7e25cb749562c91e7e44b
nshidayatullah <127877651+nshidayatullah@users.noreply.github.com>
2026-01-04 14:32:00 +0800

    Fix: Resolved lint warning in FisioterapisLayout
    - Added eslint-disable comment for useEffect dependency
    - All lint errors now resolved
    - Clean build ready for production

--- src/pages/fisioterapis/FisioterapisLayout.jsx
+++ src/pages/fisioterapis/FisioterapisLayout.jsx
@@ -18,6 +18,7 @@ const FisioterapisLayout = () => {
 
   useEffect(() => {
     checkAuth();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const checkAuth = async () => {
