8966442cd1195942e52b4184d31e29a714ae0903
nshidayatullah <127877651+nshidayatullah@users.noreply.github.com>
2026-04-13 16:06:54 +0800

    style: implement sticky footer to keep it at the bottom of the page

--- src/pages/fisioterapis/FisioterapisLayout.jsx
+++ src/pages/fisioterapis/FisioterapisLayout.jsx
@@ -154,7 +154,7 @@ const FisioterapisLayout = () => {
         </div>
       </div>
 
-      <div className="lg:pl-72 relative z-10">
+      <div className="lg:pl-72 relative z-10 flex flex-col min-h-screen">
         {/* Top header */}
         <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-white/5 bg-gray-900/50 backdrop-blur-xl px-4 shadow-sm sm:gap-x-6 lg:px-8">
           <button type="button" onClick={() => setSidebarOpen(true)} className="-m-2.5 p-2.5 text-gray-400 lg:hidden">
@@ -175,7 +175,7 @@ const FisioterapisLayout = () => {
         </div>
 
         {/* Page content */}
-        <main className="py-8">
+        <main className="py-8 flex-grow">
           <div className="px-4 sm:px-6 lg:px-8">
             <PageTransition>
               <Outlet />
